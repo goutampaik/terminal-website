b01e723b2521aebafd27f3b03fa5bbad6e95392f
goutampaik <goutampaik752@gmail.com>
2022-08-20 17:19:20 +0600

    first commit

---
+++ js/commands.js
@@ -0,0 +1,117 @@
+var mint = "https://www.helahi.com/";
+var youtube = "https://www.youtube.com/";
+var twitter = "https://www.twitter.com/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/";
+var instagram = "https://www.instagram.com/";
+var github = "https://github.com/";
+var email = 'mailto:goutampaik752@gmail.com';
+
+ddcroadmapphase1 = [
+    "<br>",
+    "Dangerous Donkeys will escape onto the blockchain, beware we estimate 5,555 will cause ",
+    "havoc. Once you get your Donkey you will be apart of the community where you will see the",
+    "P2E in phase 3.",
+    "<br>"
+];
+ddcroadmapphase2 = [
+    "<br>",
+    "More animals are coming but wait… So will the hunters. Here you will see the P2E beta!",
+    "<br>"
+];
+ddcroadmapphase3 = [
+    "<br>",
+    "The game begins… ",
+    "<br>"
+];
+team = [
+    "<br>",
+    "DDC is brought to you by a special team coming from all walks of life",
+    "Malik Mohammed",
+    "Co-Founder",
+    "Firas Ali",
+    "Co-Founder",
+    "Ahmed Malik",
+    "Head of Marketing",
+    "Rizwan Sheikh",
+    "Head of cyber security",
+    "Mathew Hope",
+    "Artist",
+    "Akash",
+    "Developer",
+    "<br>",
+    "More team and details coming",
+    "<br>"
+];
+ddcgame = [
+    "<br>",
+    "More information coming but word on the street is:",
+    "<br>",
+    "- .... . / .- -. .. -- .- .-.. ... / .-- .. .-.. .-.. / -... . / .... ..- -. - . -..",
+    "<br>"
+];
+ddcpublicsale = [
+    "<br>",
+    "botton to mint",
+    "<br>"
+];
+ddcprivatesale = [
+    "<br>",
+    "button to mint for whitelist",
+    "<br>"
+];
+
+
+social = [
+    "<br>",
+    'youtube        <a href="' + youtube + '" target="_blank">youtube/dunnky' + "</a>",
+    'twitter        <a href="' + twitter + '" target="_blank">twitter/dunnky' + '</a>',
+    'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/dunnky' + "</a>",
+    'instagram      <a href="' + instagram + '" target="_blank">instagram/dunnky' + '</a>',
+    'github         <a href="' + github + '" target="_blank">github/dunnky' + "</a>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+];
+
+
+help = [
+    "<br>",
+    '<span class="command">ddcroadmapphase1</span>',
+    '<span class="command">ddcroadmapphase2</span>',
+    '<span class="command">ddcroadmapphase3</span>',
+    '<span class="command">team</span>',
+    '<span class="command">ddcgame</span>',
+    '<span class="command">ddcpublicsale</span>',
+    '<span class="command">ddcprivatesale</span>',
+    '<span class="command">mint</span>',
+    '<span class="command">social</span>',
+    '<span class="command">help</span>',
+    '<span class="command">email</span>',
+    '<span class="command">clear</span>',
+    '<span class="command">banner</span>',
+    "<br>",
+];
+
+banner = [
+    '<span class="index"></span>',
+
+    " .----------------.  .----------------.  .-----------------. .-----------------. .----------------.  .----------------. ",
+    "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |",
+    "| |  ________    | || | _____  _____ | || | ____  _____  | || | ____  _____  | || |  ___  ____   | || |  ____  ____  | |",
+    "| | |_   ___ `.  | || ||_   _||_   _|| || ||_   \|_   _| | || ||_   \|_   _| | || | |_  ||_  _|  | || | |_  _||_  _| | |",
+    "| |   | |   `. / | || |  | |    | |  | || |  |   \ | |   | || |  |   \ | |   | || |   | |_/ /    | || |   \ \  / /   | |",
+    "| |   | |    | | | || |  | '    ' |  | || |  | |\ \| |   | || |  | |\ \| |   | || |   |  __'.    | || |    \ \/ /    | |",
+    "| |  _| |___.' / | || |   \ `--' /   | || | _| |_\   |_  | || | _| |_\   |_  | || |  _| |  \ \_  | || |    _|  |_    | |",
+    "| | |________.'  | || |    `.__.'    | || ||_____|\____| | || ||_____|\____| | || | |____||____| | || |   |______|   | |",
+    "| |              | || |              | || |              | || |              | || |              | || |              | |",
+    "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |",
+    " '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'",
+
+    '<span class="color2">Welcome to our Dunnky NFTs Collection.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
